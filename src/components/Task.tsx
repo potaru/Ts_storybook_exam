f4ccbd47dac9d094bc2c0e971b1c7905b9f995e5
potaru <jspaulbhang@gmail.com>
2021-05-11 19:14:16 +0900

    fix archived task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -14,12 +14,12 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
 	return (
 		<div className={`list-item ${state}`}>
 			<label className="checkbox">
-				<input
+				{state !== "TASK_ARCHIVED" && <input
 					type="checkbox"
 					defaultChecked={state === "TASK_ARCHIVED"}
 					disabled={true}
 					name="checked"
-				/>
+				/>}
 				<span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
 			</label>
 			<div className="title">
